1c6d4a7c2aef71adbbb704eef32c6c4513be6fde
Ravi Kerketta <rk.ravikerketta@gmail.com>
2024-02-24 18:17:21 +0530

    Update client.js
    updated for resilve error

--- src/components/auth/client.js
+++ src/components/auth/client.js
@@ -1,6 +1,1 @@
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = // supabase url
-const supabaseKey = //supabase key
-export const supabase = createClient(supabaseUrl, supabaseKey)
